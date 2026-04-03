02dd5bbe80a11f0488fc566922a4b991b59ecc43
Jonas Lindau <jonas@jonika.nu>
2026-04-03 12:22:40 +0200

    Initial Commit

---
+++ firerisk-mac/firerisk_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  firerisk_macApp.swift
+//  firerisk-mac
+//
+//  Created by Jonas Lindau on 2026-04-03.
+//
+
+import SwiftUI
+
+@main
+struct firerisk_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
