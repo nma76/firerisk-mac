1a0da02f20007ecd0c85202c03f9c2b339717e0f
Jonas Lindau <jonas@jonika.nu>
2026-04-05 13:35:57 +0200

    Created a better folder structure

--- firerisk-mac/App/Models/FireRiskModels.swift
+++ firerisk-mac/App/Models/FireRiskModels.swift
@@ -1,9 +1,15 @@
 //
-//  Forecast.swift
+//  FireRiskResponse.swift
 //  firerisk-mac
 //
 //  Created by Jonas Lindau on 2026-04-05.
 //
+
+struct FireRiskResponse: Codable {
+    let periodStartDate: String
+    let periodEndDate: String
+    let forecast: Forecast
+}
 
 struct Forecast: Codable {
     let date: String
